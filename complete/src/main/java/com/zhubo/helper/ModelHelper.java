9bbc9a6adc5c3f09bf8756d9442f6904e951a31c
Xiao Li <xiao.li@hulu.com>
2016-09-17 17:06:59 +0800

    use pay period cache

--- complete/src/main/java/com/zhubo/helper/ModelHelper.java
+++ complete/src/main/java/com/zhubo/helper/ModelHelper.java
@@ -2,10 +2,12 @@ package com.zhubo.helper;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import com.google.common.collect.Maps;
 import com.zhubo.entity.Anchor;
 import com.zhubo.entity.AnchorMetricByMinutes;
 import com.zhubo.entity.Audience;
@@ -92,9 +94,94 @@ public class ModelHelper {
             return null;
         } else {
             return pays.get(0);
-        }
-        
+        }  
     }
     
+    public static Map<Long, Map<Long, PayPeriodObject>> getAllLatestPayPeriod(ResourceManager rm, int platformId) {
+        Session session = rm.getDatabaseSession();
+        Map<Long, Map<Long, PayPeriodObject>> payCache = Maps.newHashMap();
+        Query query = session.createQuery("from AudiencePayPeriod where platform_id = :platform_id order by record_effective_time desc");
+        query.setParameter("platform_id", platformId);
+        List<AudiencePayPeriod> payRecords = query.list();
+        for(AudiencePayPeriod payRecord : payRecords) {
+            putPayPeriodInCache(payCache, payRecord.getAudienceId(), payRecord.getAnchorId(), 
+                    new PayPeriodObject(payRecord.getPlatformId(), payRecord.getMoney(), payRecord.getPeriodStart(), payRecord.getRecordEffectiveTime()));
+        }
+        return payCache;
+    }
     
+    public static void setAllLatestPayPeriod(ResourceManager rm, Map<Long, Map<Long, PayPeriodObject>> cache, int platformId) {
+        for(Long audienceId : cache.keySet()) {
+            Map<Long, PayPeriodObject> audienceCache = cache.get(audienceId);
+            for(Long anchorId : audienceCache.keySet()) {
+                PayPeriodObject payPeriod = audienceCache.get(anchorId);
+                AudiencePayPeriod oldRecord = getLatestPayPeriod(rm, audienceId, anchorId);
+                if(oldRecord == null) {
+                    AudiencePayPeriod record = new AudiencePayPeriod(audienceId, anchorId, payPeriod.platformId, payPeriod.money, 
+                            payPeriod.recordEffectiveTime, payPeriod.periodStart);
+                    rm.getDatabaseSession().save(record);                    
+                } else {
+                    oldRecord.setPlatformId(platformId);
+                    oldRecord.setMoney(payPeriod.money);
+                    oldRecord.setPeriodStart(payPeriod.periodStart);
+                    oldRecord.setRecordEffectiveDate(payPeriod.recordEffectiveTime);
+                    rm.getDatabaseSession().update(oldRecord); 
+                }
+            }
+        }
+        rm.commit();
+    }
+    
+    public static class PayPeriodObject {
+        public int platformId;
+        public int money;
+        public Date periodStart;
+        public Date recordEffectiveTime;
+        public PayPeriodObject (int platformId, int money, Date periodStart, Date recordEffectiveTime) {
+           this.platformId = platformId;
+           this.money = money;
+           this.periodStart = periodStart;
+           this.recordEffectiveTime = recordEffectiveTime;
+        }
+    }
+    
+    private static void putPayPeriodInCache(Map<Long, Map<Long, PayPeriodObject>> payCache, long audienceId, long anchorId, 
+            PayPeriodObject payPeriod) {
+        if(!payCache.containsKey(audienceId)) {
+            payCache.put(audienceId, Maps.newHashMap());
+        }
+        Map<Long, PayPeriodObject> audiencePays = payCache.get(audienceId);
+        audiencePays.put(anchorId, payPeriod);
+    }
+       
+    public static int getDiffMoneyAndUpdatePayPeriodInCache(Map<Long, Map<Long, PayPeriodObject>> payCache, long audienceId, long anchorId, PayPeriodObject payPeriod) {
+        PayPeriodObject oldPayPeriod = getPayPeriodFromCache(payCache, audienceId, anchorId);
+        if(oldPayPeriod == null) {
+            putPayPeriodInCache(payCache, audienceId, anchorId, payPeriod);
+            return 0;
+        } else if (oldPayPeriod.money < payPeriod.money) {
+            putPayPeriodInCache(payCache, audienceId, anchorId, payPeriod);
+            int diffMoney = payPeriod.money - oldPayPeriod.money;
+            return diffMoney;
+        } else if (oldPayPeriod.money > payPeriod.money) {
+            putPayPeriodInCache(payCache, audienceId, anchorId, payPeriod);
+            return payPeriod.money;
+        } else {
+            return 0;
+        }
+    }
+    
+    private static PayPeriodObject getPayPeriodFromCache(Map<Long, Map<Long, PayPeriodObject>> payCache, long audienceId, long anchorId) {
+        if(payCache.containsKey(audienceId)) {
+            Map<Long, PayPeriodObject> audiencePays = payCache.get(audienceId);
+            if(audiencePays.containsKey(anchorId)) {
+                return audiencePays.get(anchorId);
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+        
+    } 
 }
